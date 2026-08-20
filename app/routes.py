376406b6e8cc000620e69bd421ea6e4cca5ba65b
minesweepr <gabscosta29@gmail.com>
2026-08-20 15:38:48 -0300

    add paginas bases (presença, listar, checar, editar e cadastrar)

--- app/routes.py
+++ app/routes.py
@@ -4,3 +4,23 @@ from flask import render_template
 @app.route('/')
 def index():
     return render_template('teste.html')
+
+@app.route('/presenca')
+def presenca():
+    return render_template('presenca.html')
+
+@app.route('/usuarios')
+def usuarios():
+    return render_template('usuarios.html')
+
+@app.route('/informacoes_usuario')
+def informacoes_usuario():
+    return render_template('informacoes_usuario.html')
+
+@app.route('/editar_usuario')
+def editar_usuario():
+    return render_template('editar_usuario.html')
+
+@app.route('/cadastrar_usuario')
+def cadastrar_usuario():
+    return render_template('cadastrar_usuario.html')
